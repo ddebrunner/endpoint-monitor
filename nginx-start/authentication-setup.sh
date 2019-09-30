b515bf29987946a5ce9b2685e17c0f47d1978b9b
Dan Debrunner <djd@debrunners.com>
2019-09-30 20:42:58 +0000

    Set variable based upon secret

--- nginx-start/authentication-setup.sh
+++ nginx-start/authentication-setup.sh
@@ -1,6 +1,14 @@
-SECRET_HTACCESS=/var/run/secrets/streams-endpoint-monitor/server-auth/.htaccess
+SERVER_AUTH=/var/run/secrets/streams-endpoint-monitor/server-auth
+
+SECRET_HTACCESS=${SERVER_AUTH}/.htaccess
 if [ -f "${SECRET_HTACCESS}" ]; then
     /bin/cp \
         /opt/app-root/src/nginx-optional-cfg/auth_basic.conf \
         /var/opt/streams-endpoint-monitor/authentication.basic.conf
+fi
+
+SIGNATURE_SECRET=${SERVER_AUTH}/signature-secret
+if [ -f "${SIGNATURE_SECRET}" ]; then
+    secret=`cat ${SIGNATURE_SECRET}
+    echo 'set $signatureSecret ' "'${secret}'" ';' > /var/opt/streams-endpoint-monitor/authentication.signature-secret.conf
 fi
